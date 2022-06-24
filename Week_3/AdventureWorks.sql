d1810b1dd98799f3483afd35c74eb47ba5b10321
LongDT205096 <91714440+LongDT205096@users.noreply.github.com>
2022-06-24 19:48:39 +0700

    Update AdventureWorks.sql

--- Week_3/AdventureWorks.sql
+++ Week_3/AdventureWorks.sql
@@ -11,4 +11,16 @@ SELECT TOP 10 * FROM Person.Address;
 --6--
 SELECT AVG(Rate) FROM HumanResources.EmployeePayHistory;
 --7--
-select count(BusinessEntityID) FROM HumanResources.Employee;
+select count(BusinessEntityID) from HumanResources.Employee;
+--8--
+select d.FullName,count(OrderQty) as quantity from Sales.SalesOrderDetail as a
+	inner join 	Sales.SalesOrderHeader as b on b.SalesOrderID = a.SalesOrderID
+	inner join Sales.Customer as c on c.CustomerID = b.CustomerID
+	inner join Person.Person as d on c.PersonID = d.BusinessEntityID
+group by FullName
+having count(OrderQty) > 10
+--9--
+select b.Name,count(OrderQty) as quantity from Sales.SalesOrderDetail as a 
+  full outer join Production.Product as b on a.ProductID = b.ProductID
+group by b.Name
+having count(OrderQty) < 1
